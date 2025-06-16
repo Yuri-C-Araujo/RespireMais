76497cd295c9dca5cd964a491b3e772518bacb7c
Rubia15 <rbl3@aluno.ifal.edu.br>
2025-06-16 17:28:55 -0300

    Agora sim de verdade eu terminei

--- lib/Historico.dart
+++ lib/Historico.dart
@@ -19,17 +19,17 @@ class _HistoricoState extends State<Historico> {
           actions: [
             Container(
               margin: EdgeInsets.all(12),
-              decoration:  BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(100),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black87,
-              blurRadius: 6,
-              offset: Offset(1, 3),
-            ),
-          ],
-        ),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(100),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black87,
+                    blurRadius: 6,
+                    offset: Offset(1, 3),
+                  ),
+                ],
+              ),
               child: IconButton(
                 onPressed: () {},
                 icon: Icon(
@@ -42,9 +42,7 @@ class _HistoricoState extends State<Historico> {
           ],
         ),
         backgroundColor: Colors.white,
-        body: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
+        body: ListView(
           children: [
             Center(
               heightFactor: 0.5,
@@ -53,52 +51,22 @@ class _HistoricoState extends State<Historico> {
                 height: 280,
               ),
             ),
-            Text(
-              'OLÁ, JOÃO',
-              style: TextStyle(
-                fontSize: 28,
-                color: Colors.blue,
-                fontWeight: FontWeight.w900,
-                height: 2,
+            Center(
+              child: Text(
+                'OLÁ, JOÃO',
+                style: TextStyle(
+                  fontSize: 28,
+                  color: Colors.blue,
+                  fontWeight: FontWeight.w900,
+                  height: 2,
+                ),
               ),
             ),
             Column(
               children: [
                 Container(
                   width: 450,
-                  height:95,
-                  padding: EdgeInsets.all(18),
-                  margin: EdgeInsets.all(12),
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(12),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black38,
-                        blurRadius: 6,
-                        offset: Offset(2, 5)
-                      ),
-                    ],
-                  ),
-
-                  child:Column(
-                  children: [
-                  Center(
-                      child: Text('13/04 - Dor 5/10 | Fadiga: Moderada |Efeito Colateral: Tosse',
-                      style: TextStyle(
-                        fontSize: 20,
-                        color: Colors.black87,
-                        fontWeight: FontWeight.w900,
-                      ),
-                      ),
-                  ),
-                  ],
-                  ),
-                ),
-                Container(
-                  width: 450,
-                  height:95,
+                  height: 95,
                   padding: EdgeInsets.all(18),
                   margin: EdgeInsets.all(12),
                   alignment: Alignment.center,
@@ -109,48 +77,14 @@ class _HistoricoState extends State<Historico> {
                       BoxShadow(
                           color: Colors.black38,
                           blurRadius: 6,
-                          offset: Offset(2, 5)
-                      ),
+                          offset: Offset(2, 5)),
                     ],
                   ),
-
-                  child:Column(
-                    children: [
-                      Align(
-                        alignment: Alignment.centerLeft,
-                        child: Text('12/04 - Dor 3/10 | Fadiga: leve',
-                          style: TextStyle(
-                            fontSize: 20,
-                            color: Colors.black87,
-                            fontWeight: FontWeight.w900,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                Container(
-                  width: 450,
-                  height:95,
-                  padding: EdgeInsets.all(18),
-                  margin: EdgeInsets.all(12),
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(12),
-                    boxShadow: [
-                      BoxShadow(
-                          color: Colors.black38,
-                          blurRadius: 6,
-                          offset: Offset(2, 5)
-                      ),
-                    ],
-                  ),
-
-                  child:Column(
+                  child: Column(
                     children: [
                       Center(
-                        child: Text('11/04 - Dor 8/10 | Fadiga Intensa |Efeito Colateral: Náusea',
+                        child: Text(
+                          '13/04 - Dor 5/10 | Fadiga: Moderada |Efeito Colateral: Tosse',
                           style: TextStyle(
                             fontSize: 20,
                             color: Colors.black87,
@@ -163,7 +97,7 @@ class _HistoricoState extends State<Historico> {
                 ),
                 Container(
                   width: 450,
-                  height:95,
+                  height: 95,
                   padding: EdgeInsets.all(18),
                   margin: EdgeInsets.all(12),
                   alignment: Alignment.center,
@@ -174,16 +108,15 @@ class _HistoricoState extends State<Historico> {
                       BoxShadow(
                           color: Colors.black38,
                           blurRadius: 6,
-                          offset: Offset(2, 5)
-                      ),
+                          offset: Offset(2, 5)),
                     ],
                   ),
-
-                  child:Column(
+                  child: Column(
                     children: [
                       Align(
                         alignment: Alignment.centerLeft,
-                        child: Text('10/04 - Dor 0/10 | Fadiga: Leve',
+                        child: Text(
+                          '12/04 - Dor 3/10 | Fadiga: leve',
                           style: TextStyle(
                             fontSize: 20,
                             color: Colors.black87,
@@ -194,34 +127,94 @@ class _HistoricoState extends State<Historico> {
                     ],
                   ),
                 ),
-              SizedBox(height: 100),
-              ElevatedButton(
-                  onPressed: (){},
-                 style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.blue,
-                   foregroundColor: Colors.white,
-                   elevation: 6,
-                   maximumSize: Size(450, 80),
-                   padding: EdgeInsets.all(18),
-                   shape: RoundedRectangleBorder(
-                     borderRadius: BorderRadius.circular(12),
-
-                   ),
-                 ),
-                child:
-                Text('Gerar PDF / Compartilhar com médico',
-
-                   style: TextStyle(
-                     fontSize: 20,
-                     fontWeight: FontWeight.w900,
-                   ),
-                 ),
-               ),
+                Container(
+                  width: 450,
+                  height: 95,
+                  padding: EdgeInsets.all(18),
+                  margin: EdgeInsets.all(12),
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(12),
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.black38,
+                          blurRadius: 6,
+                          offset: Offset(2, 5)),
+                    ],
+                  ),
+                  child: Column(
+                    children: [
+                      Center(
+                        child: Text(
+                          '11/04 - Dor 8/10 | Fadiga Intensa |Efeito Colateral: Náusea',
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.black87,
+                            fontWeight: FontWeight.w900,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  width: 450,
+                  height: 95,
+                  padding: EdgeInsets.all(18),
+                  margin: EdgeInsets.all(12),
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(12),
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.black38,
+                          blurRadius: 6,
+                          offset: Offset(2, 5)),
+                    ],
+                  ),
+                  child: Column(
+                    children: [
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          '10/04 - Dor 0/10 | Fadiga: Leve',
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.black87,
+                            fontWeight: FontWeight.w900,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 100),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue,
+                    foregroundColor: Colors.white,
+                    elevation: 6,
+                    maximumSize: Size(450, 80),
+                    padding: EdgeInsets.all(18),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                  ),
+                  child: Text(
+                    'Gerar PDF / Compartilhar com médico',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w900,
+                    ),
+                  ),
+                ),
                 SizedBox(height: 20),
               ],
             ),
           ],
-        ),
         ),
       ),
     );
