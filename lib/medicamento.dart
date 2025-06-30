dbaa5b0944b34988241257154a5a5871c05cd0cc
wevertongsc <wgsc2@aluno.ifal.edu.br>
2025-06-30 10:44:48 -0300

    Tela de apresentação final

--- lib/medicamento.dart
+++ lib/medicamento.dart
@@ -1,35 +1,24 @@
 import 'dart:ui';
 import 'package:flutter/material.dart';
 
-class Medicamento extends StatelessWidget   {
-    const Medicamento();
-
-  static List<Map<String, String>> medicamentos = [ // lista com os dados dos medicamentos
-    {
-      'nome': 'XALKORI',
-      'imagem':
-      'https://pmlive.com/wp-content/uploads/2024/02/Pfizer-Xalkori-lung-cancer.jpg',
-      'horario': '08 EM 08 HORAS',
-    },
-    {
-      'nome': 'TAGRISSO',
-      'imagem':
-      'https://pfarma.com.br/images/noticias/tagrisso-medicamento-cancer.jpg',
-      'horario': '06 EM 06 HORAS',
-    },
-
-  ];
+class Medicamento extends StatefulWidget {
+  const Medicamento({super.key});
 
   @override
-  Widget build(BuildContext context) { // retorna um widget que representa a estrutura visual da tela
+  State<Medicamento> createState() => MedicamentoState();
+}
+
+class MedicamentoState extends State<Medicamento> {
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       bottomNavigationBar: Padding(
-        padding: EdgeInsets.all(15), // espaçamento ao redor do botão
+        padding: EdgeInsets.all(15),
         child: ElevatedButton(
           onPressed: () {},
           style: ElevatedButton.styleFrom(
             backgroundColor: Colors.blue,
-            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20), // espaço interno
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(8),
             ),
@@ -45,8 +34,7 @@ class Medicamento extends StatelessWidget   {
           ),
         ),
       ),
-
-      body: Stack( // para empilhar elementos uns sobre os outros
+      body: Stack(
         children: [
           Center(
             child: Opacity(
@@ -59,39 +47,44 @@ class Medicamento extends StatelessWidget   {
               ),
             ),
           ),
-
-          Positioned.fill( //
+          Positioned.fill(
             child: BackdropFilter(
-              filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5), // desfoque por cima da imagem
+              filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
               child: Container(color: Colors.transparent),
             ),
           ),
           SafeArea(
-            child: SingleChildScrollView(
-              padding: EdgeInsets.all(15), // espaçamento interno
-              child: Column(
+            child: Padding(
+              padding: EdgeInsets.all(15),
+              child: ListView(
                 children: [
-                   SizedBox(height: 30),
-                   Text(
+                  SizedBox(height: 20),
+                  Text(
                     'MEDICAMENTOS',
                     style: TextStyle(
                       fontSize: 30,
                       fontWeight: FontWeight.bold,
                       color: Colors.blue,
                     ),
+                    textAlign: TextAlign.center,
                   ),
-
                   SizedBox(height: 20),
-
-                  Row( // organiza lado a lado
+                  Row(
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: medicamentos.map((med) { // criação da lista
-                      return ListaMedicamentos(
-                        nome: med['nome']!,
-                        imagem: med['imagem']!,
-                        horario: med['horario']!,
-                      );
-                    }).toList(), // transforma o conteúdo do map em uma lista
+                    children: [
+                      ListaMedicamentos(
+                        nome: 'XALKORI',
+                        imagem:
+                        'https://pmlive.com/wp-content/uploads/2024/02/Pfizer-Xalkori-lung-cancer.jpg',
+                        horario: '08 EM 08 HORAS',
+                      ),
+                      ListaMedicamentos(
+                        nome: 'TAGRISSO',
+                        imagem:
+                        'https://pfarma.com.br/images/noticias/tagrisso-medicamento-cancer.jpg',
+                        horario: '06 EM 06 HORAS',
+                      ),
+                    ],
                   ),
                 ],
               ),
@@ -103,23 +96,29 @@ class Medicamento extends StatelessWidget   {
   }
 }
 
-class ListaMedicamentos extends StatelessWidget { // criação do card
+class ListaMedicamentos extends StatefulWidget {
   final String nome;
   final String imagem;
   final String horario;
 
-   ListaMedicamentos({ //construtor com valores constantes
+  ListaMedicamentos({
     required this.nome,
     required this.imagem,
     required this.horario,
+    super.key,
   });
 
-  @override //container
+  @override
+  State<ListaMedicamentos> createState() => ListaMedicamentosState();
+}
+
+class ListaMedicamentosState extends State<ListaMedicamentos> {
+  @override
   Widget build(BuildContext context) {
     return Container(
       width: 150,
-      padding: EdgeInsets.all(8), // espaço interno
-      margin: EdgeInsets.all (4), // espaço externo
+      padding: EdgeInsets.all(8),
+      margin: EdgeInsets.all(4),
       decoration: BoxDecoration(
         color: Colors.white,
         borderRadius: BorderRadius.circular(12),
@@ -127,34 +126,24 @@ class ListaMedicamentos extends StatelessWidget { // criação do card
           BoxShadow(
             color: Colors.black,
             blurRadius: 4,
-            offset: Offset(2, 2),
+            offset: Offset(0, 2),
           )
         ],
       ),
       child: Column(
         children: [
           ClipRRect(
+            borderRadius: BorderRadius.circular(8),
             child: Image.network(
-              imagem,
+              widget.imagem,
               height: 80,
               width: 100,
               fit: BoxFit.cover,
-              errorBuilder: (context, error, stackTrace) {
-                return SizedBox(
-                  height: 80,
-                  width: 100,
-                  child: Center(
-                    child: Icon(Icons.error, color: Colors.red),
-                  ),
-                );
-              },
             ),
           ),
-
           SizedBox(height: 20),
-
           Text(
-            nome,
+            widget.nome,
             style: TextStyle(
               fontWeight: FontWeight.bold,
               fontSize: 20,
@@ -163,7 +152,7 @@ class ListaMedicamentos extends StatelessWidget { // criação do card
           ),
           SizedBox(height: 5),
           Text(
-            horario,
+            widget.horario,
             style: TextStyle(
               fontSize: 15,
               color: Colors.lightBlueAccent,
