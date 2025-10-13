dfdf3ba32dba9e1a05b5a133bb6efd53c07fc19c
Yuri <yca3@aluno.ifal.edu.br>
2025-10-13 15:19:53 -0300

    Tela de Assistente iniciada.

---
+++ lib/ia.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Ia extends StatefulWidget {
+  const Ia({super.key});
+
+  @override
+  State<Ia> createState() => _IaState();
+}
+
+class _IaState extends State<Ia> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+
+      ),
+    );
+  }
+}
