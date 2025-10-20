769372b6d2840a429030b192445a12bd0b168fa6
Yuri <yca3@aluno.ifal.edu.br>
2025-10-20 15:27:18 -0300

    Continuação da TELA da IA.

--- lib/ia.dart
+++ lib/ia.dart
@@ -8,12 +8,69 @@ class Ia extends StatefulWidget {
 }
 
 class _IaState extends State<Ia> {
+  TextEditingController respostaController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
-
+        toolbarHeight: 50,
+        backgroundColor: Colors.white,
+        surfaceTintColor: Colors.white,
+        elevation: 0,
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView(
+              padding: const EdgeInsets.all(20),
+              children: [
+                Center(
+                  child: Image.asset(
+                    'assets/Assistente-Respire.png',
+                    height: 250,
+                  ),
+                ),
+                const SizedBox(height: 25),
+                Center(
+                  child: Text(
+                    'O QUE VOCÊ ESTÁ SENTINDO HOJE?',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromRGBO(127, 196, 255, 50),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SafeArea(
+            top: false,
+            child: Row(
+              children: [
+                TextFormField(controller: respostaController),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue,
+                    foregroundColor: Colors.white,
+                    elevation: 8,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    minimumSize: Size(170, 60),
+                  ),
+                  child: Text(
+                    "ENVIAR",
+                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:respire_mais/ia.dart';
 import 'splash_page.dart';
 
 void main() {
-  runApp(const MaterialApp(home: SplashPage()));
+  runApp(const MaterialApp(home: Ia()));
 }
