13af84164dfaa6209f2ffbb09fb25218f43da173
weverton <wevertongsc0303@gmail.com>
2025-11-24 15:10:28 -0300

    mapa adicionado

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
-import 'splash_page.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart'; // <--- IMPORTANTE: Adicione este import
+import 'google_maps.dart';
 
 void main() {
   runApp(const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SplashPage()
+      home: GoogleMapsPage(
+        latLong: LatLng(-9.62, -36.77),
+      )
   ));
 }
